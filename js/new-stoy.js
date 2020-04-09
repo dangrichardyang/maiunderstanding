7840e260aec8cf9b564bbb684382edb1d1364ec6
Richard Yang <yangrd0133@uwec.edu>
2020-04-09 15:06:18 -0500

    commit

--- js/new-stoy.js
+++ js/new-stoy.js
@@ -1,5 +1,5 @@
 const newStoyButton = document.getElementById('new-story-button');
 
 newStoyButton.addEventListener('click', event => {
-    window.open("../project-1/stories/its-about-time.html");
+    window.open("stories/its-about-time.html");
   });
